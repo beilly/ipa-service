c3a9499e850d3364b3c8e0443e9f696208158ece
benli.shi <shibl@2345.com>
2018-10-30 11:41:52 +0800

    添加plist支持

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('plist', views.plist, name='index'),
     path('', views.index, name='index'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,21 @@ import requests
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
+
+
+def plist(request):
+    with open('static/dkw_ios_test.plist', 'r', encoding='UTF-8') as f:
+        c = f.read()
+    response = HttpResponse(c)
+
+    file_name = "ios_test.plist"
+    response['Content-Type']='application/octet-stream'
+    response['Content-Disposition']='attachment;filename="{0}"'.format(file_name)
+    
+    return response
