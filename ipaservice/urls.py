cd8673c5103b8f888523af988adec8d58eda6c60
benli.shi <shibl@2345.com>
2018-10-31 15:11:44 +0800

    添加plist获取指定版本

--- ipaservice/urls.py
+++ ipaservice/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
-    path('', include('polls.urls')),
+    path(r'^$', include('polls.urls')),
+    url('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,33 @@
-from django.shortcuts import render
 import requests
 from django.http import HttpResponse
+import logging
 
 # Create your views here.
+logger = logging.getLogger("django")
 
 
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
+    logger.info(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 
-def plist(request):
+def plist(request, ipa_id):
     with open('static/dkw_ios_test.plist', 'r', encoding='UTF-8') as f:
-        c = f.read()
-    response = HttpResponse(c)
+        c = f.readlines()
+
+    logger.info(ipa_id)
+
+    result=[]
+    for line in c:
+        if '{{id}}' in line:
+            line = line.replace('{{id}}', ipa_id)
+        result.append(line)
+
+    response = HttpResponse(result)
 
     file_name = "ios_test.plist"
-    response['Content-Type']='application/octet-stream'
-    response['Content-Disposition']='attachment;filename="{0}"'.format(file_name)
-    
+    response['Content-Type'] = 'application/octet-stream'
+    response['Content-Disposition'] = 'attachment;filename="{0}"'.format(file_name)
+
     return response
